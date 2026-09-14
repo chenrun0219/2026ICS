9e81ed6d151c2ed19f1d1bc06f76b76740215a98
chenrun0219 <2682886635@qq.com>
2026-09-14 23:53:48 +0800

    update main

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ int main()
 {
     // @TODO: print a sentence you want.
     printf("This is just a test.\n");
+    printf("The changes in main chain.\n");
     printf("Hello, world!\n");
     return 0;
 }
