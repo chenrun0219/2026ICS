b1e384b9c9a9496633f018907aefe2493d51f3d0
chenrun0219 <2682886635@qq.com>
2026-09-15 11:28:04 +0800

    update on feature

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main()
     // @TODO: print a sentence you want.
     printf("This is just a test.\n");
     printf("The changes in main chain.\n");
+    printf("This line is from the feature branch.\n");
     printf("Hello, world!\n");
     return 0;
 }
