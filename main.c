f65dfac6b8d4a28f895e40ab42ae55ae7d9b873a
chenrun0219 <2682886635@qq.com>
2026-09-15 11:32:40 +0800

    update on main

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main()
 {
     // @TODO: print a sentence you want.
     printf("This is just a test.\n");
-    printf("The changes in main chain.\n");
+    printf("This line is from main branch.\n");
     printf("Hello, world!\n");
     return 0;
 }
